55b4e1208ccedb77c6d529bc4d8a26759b4814b1
ehtrellik <155147710+ehtrellik@users.noreply.github.com>
2023-12-31 17:51:10 +0800

    Update index.js
    testing frontpage for ruthless

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import BetrayalCheatSheet from "./components/BetrayalCheatSheet";
+import BetrayalCheatSheet from "./components/BetrayalCheatSheetRuthless";
 
 ReactDOM.render(<BetrayalCheatSheet />, document.getElementById('root'));
